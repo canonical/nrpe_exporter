a732716362f092a9d0b329eb906a63a0413c3933
Ryan Barry <ryan.barry@canonical.com>
2023-02-16 23:57:26 +0000

    Trim the buffer before logging

--- nrpe_exporter.go
+++ nrpe_exporter.go
@@ -70,7 +70,7 @@ func collectCommandMetrics(cmd string, conn net.Conn, logger log.Logger) (Comman
 	ipaddr, _, err := net.SplitHostPort(conn.RemoteAddr().String())
 	level.Info(logger).Log("msg", "Command returned", "command", cmd,
 		"address", ipaddr, "duration", duration, "return_code", result.ResultCode,
-		"command_output", fmt.Sprintf("%s", result.CommandBuffer))
+		"command_output", fmt.Sprintf("%s", result.CommandBuffer[:]))
 	statusOk := 1.0
 	if result.ResultCode != 0 {
 		statusOk = 0
